36aadeac243ce8ebaa5f6d0b8bd8d74a7be5833a
Jeremyvb <102309847+Jeremyvb@users.noreply.github.com>
2022-06-20 01:10:45 -0400

    Delete EP4 directory

--- EP4/DietaSanaPOO/src/main/java/ClasesYObjetos/Calculadora.java
+++
@@ -1,8 +0,0 @@
-package ClasesYObjetos;
-
-public interface Calculadora {
-      
-    public abstract float calcularHB(String Genero, float Peso, float Altura, int Edad);
-    public abstract float calcularImc(float Peso, float Altura);
- 
-}
